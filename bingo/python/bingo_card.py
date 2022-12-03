93b8aebfbe140f52ed8ec99c57381b619dae69e4
Ryan Schouweiler <reschouw@gmail.com>
2022-12-03 11:11:33 -0800

    Tracking called numbers
    [#183952164]

--- bingo/python/bingo_card.py
+++ bingo/python/bingo_card.py
@@ -4,24 +4,75 @@ class card:
     """ Class for a bingo card"""
     
     def __init__(self):
-        """ Creates an empty bingo card """
-        self.grid = {
+        """
+        Creates an empty bingo card
+        """
+        # The numbers on the card
+        self.numbers = {
             "b" : sample(range(1 , 16), 5),
             "i" : sample(range(16, 31), 5),
             "n" : sample(range(31, 46), 5),
             "g" : sample(range(31, 46), 5),
             "o" : sample(range(61, 76), 5),
         }
-        self.grid["n"][2] = "Free"
-        
+        self.numbers["n"][2] = "Free"
 
+        # The status of a square: marked or unmarked
+        self.marks = {
+            "b" : [False] * 5,
+            "i" : [False] * 5,
+            "n" : [False] * 5,
+            "g" : [False] * 5,
+            "o" : [False] * 5,
+        }
+        self.marks["n"][2] = True
+
+        
+    #TODO: Rotate this output 90 degrees
     def __str__(self):
+        """
+        Returns string representation of a bingo card
+        """
         text = ""
-        for column in self.grid:
+        for column in self.numbers:
             for row in range(5):
-                text = text + str(self.grid[column][row]) + " "
+                text = text + str(self.numbers[column][row]) + " "
             text += "\n"
         return text
 
     def get_square(self, column, row):
-        return self.grid[column][row]
+        """
+        Returns the number (or "Free") in a given square
+        """
+        return self.numbers[column][row]
+    
+    def set_square(self, column, row, value):
+        """
+        Sets the number in a specific square
+        Used for testing, or if a player wants to choose their own numbers
+        Can be used to override the "Free" square
+        """
+        self.numbers[column][row] = value
+
+    def is_marked(self, column, row):
+        """
+        Returns true if square has been marked/called
+        The "Free" square is marked by default on card creation
+        """
+        return self.marks[column][row]
+    
+    def mark(self, column, row):
+        """
+        Marks that a square has been called
+        """
+        self.marks[column][row] = True
+
+    def call(self, column, number):
+        """
+        Similar to mark(), marks a square if it has been called
+        However, this only makes a mark if the called number is on the card
+        """
+        if number in self.numbers[column]:
+            row = self.numbers[column].index(number)
+            self.mark(column, row)
+
--- bingo/python/test_bingo_card.py
+++ bingo/python/test_bingo_card.py
@@ -33,11 +33,34 @@ def test_check_squares_are_valid():
             if square == "Free":
                 continue
             assert(square in rules[column])
-            
+    assert(card.get_square("n", 2) == "Free")
 
-    
-#test
-#def test_get_card():
-#    board = bingo_card.card()
-#    empty_state = [[False for x in range(10)] for y in range(10)]
-#    assert(board.get() == empty_state)
+# Set the value of a square and check it
+def test_set_square_number():
+    card = bingo_card.card()
+    card.set_square("o", 0, 65)
+    assert(card.get_square("o", 0) == 65)
+    card.set_square("n", 2, 40)
+    assert(card.get_square("n", 2) == 40)
+    card.set_square("b", 4, 15)
+    assert(card.get_square("b", 4) == 15)
+
+# Make sure that the "Free" square is marked by default
+def test_free_marked():
+    card = bingo_card.card()
+    assert(card.is_marked("n", 2))
+
+# Make sure that the "Free square is marked by default"
+def test_marking():
+    card = bingo_card.card()
+    assert(not card.is_marked("o", 4))
+    card.mark("o", 4)
+    assert(card.is_marked("o", 4))
+
+# Mark that a number has been called
+def test_number_called():
+    card = bingo_card.card()
+    square_number = card.get_square("b", 3)
+    assert(not card.is_marked("b", 3))
+    card.call("b", square_number)
+    assert(card.is_marked("b", 3))
